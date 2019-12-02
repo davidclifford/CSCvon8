d4d728361a69f9742c363d8a67a5088aaedb834d
David Clifford <david.clifford@freestyleiot.com>
2019-12-02 13:35:58 +1100

    Example Arduino interupt controled monitor

--- Examples/div10-16.s
+++ Examples/div10-16.s
@@ -9,8 +9,19 @@
     STO     B num+1
 start:
     LDA     num
-    LDB     num+1
+    LDB     $99
+    STO     A*B mul1
+    STO     A*BHI mul2
+
+    LDA     num+1
+    LDB     $19
+    STO     A*B mul3
+    STO     A*BHI mul4
 
     JMP     $FFFF
 
 num:    EQU $f000
+mul1:   EQU $f002
+mul2:   EQU $f003
+mul3:   EQU $f004
+mul4:   EQU $f005
