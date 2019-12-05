2ae5108d38dcba50f10f4c313913150ed55ed471
davidclifford <david.clifford.142@gmail.com>
2019-12-05 18:47:16 +1100

    .gitignore fix

--- VGA/pic_converter.py
+++ VGA/pic_converter.py
@@ -14,10 +14,10 @@ for y in range(512):
     for x in range(256):
         try:
             pix = pixels[x, y]
-            red = pix[0] >> 5
-            grn = pix[1] >> 5
+            red = pix[0] >> 6
+            grn = pix[1] >> 6
             blu = pix[2] >> 6
-            colour = red | grn << 3 | blu << 6
+            colour = red << 4 | grn << 2 | blu << 0
             print(x, y, red, grn, blu, colour)
             pic_file.write(uint8(colour))
         except IndexError:
