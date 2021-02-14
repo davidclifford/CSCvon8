1b48d14760775bb6c1ce9bb34d0cd5c08f6c9122
David Clifford <david.clifford.142@gmail.com>
2021-02-14 22:27:12 +1100

    Maze game - display maze 2d on screen

--- Games/maze.s
+++ Games/maze.s
@@ -1,12 +1,29 @@
 # Maze Generator
 # David Clifford Feb 2021
 #
+
+#define GRID_SIZE @7
+#define GRID_COLOUR $30
+#define GRID_COLOUR1 $30
+#define GRID_COLOUR2 $30
+#define GRID_X @4
+#define GRID_Y @4
+
+#define NORTH @1
+#define EAST @2
+#define SOUTH @4
+#define WEST @8
+
+#define WAIT #JIU .; INA
+
 start:
 # Clear screen
-#    STO 0 __paper
-#    JSR sys_cls sys_cls_ret
+    STO 0 __paper
+    JSR sys_cls sys_cls_ret
 
-# Maze is 16 x 16
+#########################
+# Generate Maze 16 x 16 #
+#########################
 # Empty maze
     LDB 0
 1:
@@ -88,7 +105,7 @@ start:
     LDA sp
     STO A+1 sp
 
-    #   r = rand(c) ## r = 0..(c-1)
+    #   r = rand(c)
     JSR sys_rand sys_rand_ret
     LDA __rand_seed
     LDB c
@@ -128,7 +145,7 @@ start:
     LDB c
     LDB A+B
     LDA dy,B
-    STO A yy
+    ##STO A yy
     #   y = y + yy
     LDB y
     STO A+B y
@@ -178,12 +195,156 @@ start:
     # goto next
     JMP 4b
 2:
-# Return to OS
-    JMP sys_cli
+#############
+# Draw Maze #
+#############
+
+    # Cells  x & y are top left coords
+    # Draw border top and left
+    LCA GRID_SIZE
+    LCB @16
+    STO A*B wid
+    # for i = 0 to 16
+    LCA GRID_X
+    STO A x
+    LCA GRID_Y
+    STO A y
+    STO 0 i
+1:
+    #   plot x+i,y,colour
+    LDA x
+    LDB i
+    LDB A+B
+    LCA GRID_COLOUR
+    STI A y,B
+    #   plot x,y+i,colour
+    LDA y
+    LDB i
+    STO A+B yy
+    LDB x
+    LCA GRID_COLOUR
+    STI A yy,B
+    # next i
+    LDA i
+    LDA A+1
+    STO A i
+    LDB wid
+    JLO 1b
+##############
+# Draw walls #
+##############
+    # yy = GRID_SIZE-1
+    LCA GRID_SIZE
+    LCB GRID_Y
+    LDA A+B
+    STO A yy
+    # for y=0 to 15
+    STO 0 y
+    #   xx = GRID_SIZE-1
+1:
+    LCA GRID_SIZE
+    LCB GRID_X
+    LDA A+B
+    STO A xx
+    #   for x=0 to 15
+    STO 0 x
+8:
+    #     c = maze[y*16+x]
+    LDA y
+    LCB @16
+    LDA A*B
+    LDB x
+    LDB A+B
+    LDA maze,B
+    STO A c
+    #     when c & EAST == 0
+    LCB EAST
+    LDA A&B
+    JAZ 2f
+4:
+    #     when c & SOUTH == 0
+    LDA c
+    LCB SOUTH
+    LDA A&B
+    JAZ 3f
+    JMP 7f
+    # Draw East wall
+2:
+    #        for i=0 to GRID_SIZE-1
+    LCA GRID_SIZE
+    STO A i
+    #          plot xx,yy+i
+5:
+    LDB xx
+    LCA GRID_COLOUR1
+    STI A yy,B
+    WAIT ##################
+    LDA yy
+    STO A-1 yy
+    LDA i
+    STO A-1 i
+    JAZ 6f
+    JMP 5b
+6:
+    LDA yy
+    LCB GRID_SIZE
+    STO A+B+1 yy
+    JMP 4b
+    # Draw South wall
+3:
+    #        for i=0 to GRID_SIZE-1
+    LCA GRID_SIZE
+    STO A i
+    #          plot xx+i, yy
+5:
+    LDB xx
+    LCA GRID_COLOUR2
+    STI A yy,B
+    WAIT ###################
+    STO B-1 xx
+    LDA i
+    STO A-1 i
+    JAZ 4f
+    JMP 5b
+4:
+    LDA xx
+    LCB GRID_SIZE
+    STO A+B+1 xx
+7:
+    #     xx += GRID_SIZE
+    LDA xx
+    LCB GRID_SIZE
+    STO A+B xx
+
+    #   next x
+    LDA x
+    STO A+1 x
+    LCB @15
+    JLO 8b
+
+    #   yy += GRID_SIZE
+    LDA yy
+    LCB GRID_SIZE
+    STO A+B yy
+
+    # next y
+    LDA y
+    STO A+1 y
+    LCB @15
+    JLO 1b
+
+# Wait for keypress
+    JIU .
+    INA
+    LCB 'q'
+    JEQ sys_cli
+    JMP start
 
 PAG
 choice: BYTE @4
 # Directions = N E S W
+dx: HEX "00 01 00 FF"
+dy: HEX "FF 00 01 00"
 x:  BYTE
 y:  BYTE
 c:  BYTE
@@ -193,8 +354,8 @@ sp: BYTE
 xx: BYTE
 yy: BYTE
 addr: BYTE
-dx: HEX "00 01 00 FF"
-dy: HEX "FF 00 01 00"
+wid: BYTE
+
 PAG
 maze: BYTE
 PAG
