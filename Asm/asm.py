fdd6b4ea8f8046d0697ac4479d04d95c494d2374
David Clifford <david.clifford.142@gmail.com>
2023-02-19 23:53:29 +1100

    Start on python based assembler

--- Asm/asm.py
+++ Asm/asm.py
@@ -1,2 +1,18 @@
 #  Python Assembler Started 26/06/2021
+with open('monitor.s') as f:
+    lines = f.readlines()
 
+for l in lines:
+    l = l.rstrip().lstrip().lower().replace('\t', '')
+    # convert strings to HEX
+    m = l.split('"')
+    if len(m) > 1:
+        l = m[0]
+        n = m[1].replace("\\n", "\n").replace("\\r", "\r").replace("\\t", "\t")
+        for c in n:
+            l = l + hex(ord(c)).replace("0x", "$") + " "
+    # remove comments
+    l = l.split("#")[0]
+    for c in l:
+        print(c, end='')
+    if len(l) != 0: print()
